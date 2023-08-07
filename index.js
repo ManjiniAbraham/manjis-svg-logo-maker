6bef32ae8e7ed0a6e28efec61536e55d3c0a20d2
manjini <manjini.abraham@gmail.com>
2023-08-07 16:20:40 -0400

    updated index.js

--- index.js
+++ index.js
@@ -34,3 +34,59 @@ const questions = [
     message: 'Enter the shape color (keyword or hexadecimal number):',
   },
 ]
+// Instantiate the appropriate shape class, get its SVG content based on user input
+function generateSVG(text, textColor, shape, shapeColor) {
+    let shapeInstance;
+    let shapeSVG = '';
+  
+    switch (shape) {
+      case 'circle':
+        shapeInstance = new Circle(shapeColor);
+        shapeSVG = shapeInstance.getSVGContent();
+        break;
+      case 'triangle':
+        shapeInstance = new Triangle(shapeColor);
+        shapeSVG = shapeInstance.getSVGContent();
+        break;
+      case 'square':
+        shapeInstance = new Square(shapeColor);
+        shapeSVG = shapeInstance.getSVGContent();
+        break;
+      default:
+        throw new Error('Invalid shape selected.');
+    }
+  // Construct the SVG content by combining shape-specific SVG and text elements
+    const svgContent = `
+      <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shapeSVG}
+      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-size="50">${text}</text>
+      </svg>
+    `;
+  
+    return svgContent;
+  }
+  // Save the generated SVG content to a file 
+  function saveSVGToFile(fileName, svgContent) {
+    fs.writeFile(fileName, svgContent, (err) => {
+      if (err) {
+        console.error('Error saving the SVG file:', err);
+      } else {
+        console.log('Generated ' + fileName);
+      }
+    });
+  }
+  // Initiate the user input process, generate SVG content, and save to a file
+    function init() {
+      inquirer
+        .prompt(questions)
+        .then((answers) => {
+          const { text, textColor, shape, shapeColor } = answers;
+          const svgContent = generateSVG(text, textColor, shape, shapeColor);
+          saveSVGToFile('logo.svg', svgContent);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+    
+    init();
